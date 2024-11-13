38fc99cef39c77f6a1081cc2e3b4be0c3f38a656
munh-orgil <munh-orgil@github.com>
2024-11-13 08:10:45 +0800

    nov 13

---
+++ 11. nov/(13) 2563. Count the Number of Fair Pairs.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+    Solution() {
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
+        cout.tie(NULL);
+    }
+    using ll = long long;
+    ll countFairPairs(vector<int>& a, int lower, int upper) {
+        int n = a.size();
+        sort(a.begin(), a.end());
+        ll ret = 0;
+        for (int i = 0; i < n; i++) {
+            ll l = lower_bound(a.begin(), a.end(), lower - a[i]) - a.begin();
+            ll r = upper_bound(a.begin(), a.end(), upper - a[i]) - a.begin();
+            while (r >= n || (r >= 0 && a[r] + a[i] > upper)) {
+                r--;
+            }
+            if (l <= r) {
+                ret += (r - l) + !(l <= i && i <= r);
+            }
+        }
+        return ret / 2;
+    }
+};
