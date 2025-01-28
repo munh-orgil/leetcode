ad4f0b26d65295f3575d3c26773b73bc92797160
munkh-orgil <munkhorgil@gerege.mn>
2025-01-28 10:38:10 +0800

    a

--- 2025/01. jan/(28) 2658. Maximum Number of Fish in a Grid.cpp
+++ 2025/01. jan/(28) 2658. Maximum Number of Fish in a Grid.cpp
@@ -13,36 +13,28 @@ public:
         int n = a.size();
         int m = a[0].size();
         vector<int> d = { 1, 0, -1, 0, 1 };
-        vector<vector<int>> vis(n, vector<int>(m, 0));
-        function<void(int, int, int)> dfs;
-        dfs = [&](int x, int y, int c) {
-            vis[x][y] = c;
+        vector<vector<bool>> vis(n, vector<bool>(m, 0));
+        function<void(int, int, int&)> dfs;
+        dfs = [&](int x, int y, int& sum) {
+            vis[x][y] = 1;
+            sum += a[x][y];
             for (int i = 0; i < 4; i++) {
                 int u = x + d[i];
                 int v = y + d[i + 1];
                 if (u < 0 || v < 0 || u >= n || v >= m || vis[u][v] || !a[u][v]) {
                     continue;
                 }
-                dfs(u, v, c);
+                dfs(u, v, sum);
             }
             };
 
-        int color = 0;
+        int ret = 0, sum;
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
                 if (!vis[i][j] && a[i][j]) {
-                    color++;
-                    dfs(i, j, color);
-                }
-            }
-        }
-        vector<int> sum(color + 1, 0);
-        int ret = 0;
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (vis[i][j]) {
-                    sum[vis[i][j]] += a[i][j];
-                    ret = max(ret, sum[vis[i][j]]);
+                    sum = 0;
+                    dfs(i, j, sum);
+                    ret = max(ret, sum);
                 }
             }
         }
